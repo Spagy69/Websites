60ee03b2fe3053190fb7365201b754e5e6eb8b63
Vít Machač <spagy@spagetak.com>
2025-11-25 13:25:55 +0100

    Refactor CSS file references and consolidate styles across projects. Started using javascript

---
+++ JavaScript Basics/script.js
@@ -0,0 +1,10 @@
+const hdr = document.getElementById("hdr");
+const btn = document.getElementById("btn");
+const colorSquare = document.getElementById("color-square");
+
+btn.onclick = () => {
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    colorSquare.style.backgroundColor = randomColor;
+    console.log(`Changed color to: ${randomColor}`);
+    document.title = `Current Color: ${randomColor}`;
+};
